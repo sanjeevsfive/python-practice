c2cdf488b52d13ed6d9c98cbc9d640d070d0b079
Sanjeev Kumar Shah <sanjeevsfive@yahoo.co.in>
2021-10-21 01:28:03 +0530

    furhter promrammed

---
+++ forlp.py
@@ -0,0 +1,22 @@
+'''
+for x in range(10):
+    print(2*x, end=",")
+a = ['sanjeev', 'amit', 'monty']
+
+for name in a:    
+    print(name*2)
+'''    
+#n = 5
+#while n >= 0:
+#    print(n)
+#    n -= 1
+
+#for i in range(10):
+ #   if i > 6:
+  #    break
+   # print(i) 
+
+for i in range(10):
+    if i == 6:
+      continue
+    print(i) 
---
+++ rangelist.py
@@ -0,0 +1,9 @@
+print('enter number to be printed')
+a = int(input())
+if a < 40:
+    c = list(range(a - 5, a, 2))
+    print(c)
+else:
+    print('enter less than 20')
+       
+
